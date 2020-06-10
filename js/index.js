badb05843d7fbd2c6ae2ee39481f2f553527359f
Tran <tran97bn@gmail.com>
2020-06-10 08:34:57 +0700

    completed home

--- js/index.js
+++ js/index.js
@@ -1,12 +1,10 @@
+// Slider
 $('.owl-carousel').owlCarousel({
     loop: true,
     margin: 0,
-    items: 4,
-    dots: false,
     nav: true,
     navText: false,
-    // ["<span class='fa fa-angle-left'><span/>", "<span class='fa fa-angle-right'><span/>"]
-    items: 4,
+    dots: false,
     responsive: {
         0: {
             items: 1,
@@ -22,6 +20,67 @@ $('.owl-carousel').owlCarousel({
 
         992: {
             items: 4,
-        }
+        },
     }
 })
+
+
+// Dropdown in Menu-mobile
+
+$(document).ready(function() {
+    $(".humberger-menu__mobile__menu .main-menu li:nth-child(2)").click(function() {
+        $(".sub-menu").toggleClass("shop-menu-open");
+        $(".fa-caret-right").toggleClass("rotale-icon");
+    });
+});
+
+
+// Buger-Menu
+
+$('.humberger, .humberger-menu__overlay').click(function() {
+    $('.humberger-menu').toggleClass('show-humberger-menu');
+    $('.humberger-menu__overlay').toggleClass('active');
+    $('.humberger').toggleClass('opened');
+    $('body').toggleClass('overflow');
+});
+
+
+// Nivo slider
+$(window).load(function() {
+    return $("#slider").nivoSlider({
+        directionNav: false,
+        controlNav: false,
+        animSpeed: 1000,
+        pauseTime: 3000,
+    });
+});
+
+
+// Back to top
+
+$('.scroll-top').click(function() {
+    $('body,html').animate({
+        scrollTop: 0
+    }, 300);
+});
+
+window.onscroll = function() {
+    scrollFunction()
+}
+
+function scrollFunction() {
+    if (document.body.scrollTop > 500 || document.documentElement.scrollTop > 200) {
+        document.getElementById("back-to-top").style.opacity = "1";
+    } else {
+        document.getElementById("back-to-top").style.opacity = "0";
+    }
+    // if (document.body.scrollTop > 700 || document.documentElement.scrollTop > 700) {
+    //     document.getElementById("header").style.background = "#fff";
+    // } else {
+    //     document.getElementById("header").style.background = "rgba(0, 0, 0, 0.6)";
+    // }
+}
+
+$('.nav-search .btn-orange').click(function() {
+    $('.mini-search').toggleClass('show-search ');
+});
